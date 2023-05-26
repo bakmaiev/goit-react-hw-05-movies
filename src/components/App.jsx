1b42590711896c379873e215f0feff6fb05cfc57
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-26 17:11:08 +0300

    Create Layout component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,18 @@ import Home from 'pages/Home';
 import MovieDetails from 'pages/MovieDetails';
 import Movies from 'pages/Movies';
 import NotFound from 'pages/NotFound';
-import { NavLink, Route, Routes } from 'react-router-dom';
-import { styled } from 'styled-components';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
 
 export const App = () => {
-  const StyledLink = styled(NavLink)`
-    color: black;
-
-    &.active {
-      color: orange;
-    }
-  `;
-
   return (
-    <>
-      <nav>
-        <StyledLink to="/">Home</StyledLink>
-        <StyledLink to="/movies">Movies</StyledLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:movieId" element={<MovieDetails />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   );
 };
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Layout;
