18d24bfa2cff66117bd6899eb548069532a95f0d
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-26 23:56:02 +0300

    Create Review component

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMoviesReviews } from 'services';
 
 const Reviews = () => {
-  return <div>Info about reviews</div>;
+  const [reviews, setReviews] = useState(null);
+  const { movieId } = useParams();
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await getMoviesReviews(movieId);
+      setReviews(data.results);
+      console.log(data.results);
+    };
+    getData();
+  }, [movieId]);
+  return (
+    reviews && (
+      <ul>
+        {reviews.map(review => {
+          return (
+            <li key={review.id}>
+              <b>{review.author}</b>
+              <p>{review.content}</p>
+            </li>
+          );
+        })}
+      </ul>
+    )
+  );
 };
 
 export default Reviews;
