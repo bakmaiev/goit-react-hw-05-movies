8295c0d68e4902acc7af36905e1a874219520c50
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-27 19:49:42 +0300

    Add new changes

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,15 +1,20 @@
+import SearchForm from 'components/SearchForm/SearchForm';
 import React, { useEffect, useState } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 import { getMoviesByQuery } from 'services';
 
 const Movies = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [moviesByQuery, setMoviesByQuery] = useState(null);
-  const [value, setValue] = useState('');
+  const location = useLocation();
+  console.log(location);
 
   const query = searchParams.get('query') ?? '';
 
   useEffect(() => {
+    if (!query) {
+      return;
+    }
     const getData = async () => {
       const { data } = await getMoviesByQuery(query);
       setMoviesByQuery(data.results);
@@ -18,25 +23,24 @@ const Movies = () => {
     query && getData();
   }, [query]);
 
+  const handleSubmitForm = e => {
+    e.preventDefault();
+    const form = e.target;
+    const { value } = form.elements.query;
+    setSearchParams(value ? { query: value } : {});
+    form.reset();
+  };
+
   return (
     <>
-      <div>
-        <input type="text" onChange={e => setValue(e.target.value.trim())} />
-        <button
-          onClick={() => {
-            setSearchParams(value ? { query: value } : {});
-          }}
-        >
-          Change SP
-        </button>
-      </div>
+      <SearchForm onSubmit={handleSubmitForm} />
       {moviesByQuery && (
         <div>
           <ul>
             {moviesByQuery.map(movie => {
               return (
                 <li key={movie.id}>
-                  <Link to={`movies/${movie.id}`}>
+                  <Link to={`${movie.id}`} state={{ from: location }}>
                     {movie.title ?? movie.name}
                   </Link>
                 </li>
