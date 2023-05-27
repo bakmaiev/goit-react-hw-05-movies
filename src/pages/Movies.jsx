8179a42bf153b2824d58590fb31e5ce441e1a91a
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-27 15:48:53 +0300

    Create Movies pages

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import { getMoviesByQuery } from 'services';
 
 const Movies = () => {
-  return <div>Movies Page</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [moviesByQuery, setMoviesByQuery] = useState(null);
+  const [value, setValue] = useState('');
+
+  const query = searchParams.get('query') ?? '';
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await getMoviesByQuery(query);
+      setMoviesByQuery(data.results);
+      console.log(data.results);
+    };
+    query && getData();
+  }, [query]);
+
+  return (
+    <>
+      <div>
+        <input type="text" onChange={e => setValue(e.target.value.trim())} />
+        <button
+          onClick={() => {
+            setSearchParams(value ? { query: value } : {});
+          }}
+        >
+          Change SP
+        </button>
+      </div>
+      {moviesByQuery && (
+        <div>
+          <ul>
+            {moviesByQuery.map(movie => {
+              return (
+                <li key={movie.id}>
+                  <Link to={`movies/${movie.id}`}>
+                    {movie.title ?? movie.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Movies;
